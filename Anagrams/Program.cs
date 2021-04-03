7817163edca5ce1e590e84c31020f795060d83ea
Chandra Sekhar Kosuru <kosuruchandra@gmail.com>
2021-04-03 09:36:17 +0530

    Code Challeng - 02042021

---
+++ Anagrams/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace Anagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testCase = Convert.ToInt32(Console.ReadLine());
+            if (testCase >= 1 && testCase <= 10)
+            {
+                var val1String = new string[testCase];
+                var val2String = new string[testCase];
+
+                for (int i = 0; i < testCase; i++)
+                {
+                    val1String[i] = Convert.ToString(Console.ReadLine());
+                    val2String[i] = Convert.ToString(Console.ReadLine());
+                }
+
+                for (int l = 0; l < testCase; l++)
+                {
+                    if ((val1String[l].Length >= 1 && val1String[l].Length <= 1000) &&
+                        (val2String[l].Length >= 1 && val2String[l].Length <= 1000))
+                    {
+                        var val1 = val1String[l].ToArray();
+                        int valueCount = 0;
+                        char lastChat = default(char);
+                        foreach (var va in val1)
+                        {
+                            var val2 = val2String[l].ToArray();
+                            foreach (var va2 in val2)
+                            {
+                                if (va2.CompareTo(va) == 0)
+                                {
+                                    if (!lastChat.Equals(va))
+                                    {
+                                        lastChat = va;
+                                        valueCount++;
+                                    }
+                                }
+                            }
+                        }
+
+                        if (valueCount == val1String[l].Length)
+                        {
+                            Console.WriteLine("YES");
+                        }
+                        else
+                        {
+                            Console.WriteLine("NO");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nofInterger = Convert.ToInt32(Console.ReadLine());
+            if (nofInterger >= 3 && nofInterger <= 1000000)
+            {
+                var values = new int[nofInterger];
+                for (int i = 0; i < nofInterger; i++)
+                {
+                    values[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                var sortedValues = values.OrderByDescending(x => x).ToArray();
+                var count = sortedValues.Length - 1;
+                for (int k = 0; k < sortedValues.Length; k++)
+                {
+                    if (k == count)
+                    {
+                        Console.WriteLine(sortedValues[k]);
+                        break;
+                    }
+                    else if (k <= count)
+                    {
+                        Console.WriteLine(sortedValues[k]);
+                        Console.WriteLine(sortedValues[count]);
+                        count--;
+                    }
+                    else if (k >= count)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
